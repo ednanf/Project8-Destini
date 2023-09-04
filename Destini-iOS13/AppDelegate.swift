5be7f67684f501fce161c8ee8da2399abb884520
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2023-09-04 13:03:17 -0300

    adjusted headers

--- Destini-iOS13/AppDelegate.swift
+++ Destini-iOS13/AppDelegate.swift
@@ -5,6 +5,8 @@
 //  Created by Angela Yu on 08/08/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
+//  Modified by Ednan R. Frizzera Filho on 09/04/2023
+//
 
 import UIKit
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -1,9 +1,11 @@
 //
-//  Sotry.swift
+//  Story.swift
 //  Destini-iOS13
 //
 //  Created by Angela Yu on 08/08/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
+//
+//  Modified by Ednan R. Frizzera Filho on 09/04/2023
 //
 
 import Foundation
